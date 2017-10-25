c7d2efd97bfd8d591a95d47fd0f04d8028236abe
Jerryhaom <32832814+Jerryhaom@users.noreply.github.com>
2017-10-25 11:44:49 +0800

    Update __init__.py

--- canova-1.0/src/canova/__init__.py
+++ canova-1.0/src/canova/__init__.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
 from canova_source import *
 
+def test()
+    x = [1,3,3,4,5,5,6,7,7,7,8,8,9]
+    y = [2,4,5,6,6,5,7,8,8,9,6,8,7]
+    print canova(x,y)
+    
 if __name__ == '__main__':
     '''
     This is a canova python package
